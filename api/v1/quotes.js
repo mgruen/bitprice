9c131cb8fe4bb21a72e118fdeaf215b63754735c
Michael Gruen <6547037+mgruen@users.noreply.github.com>
2021-03-24 22:43:38 -0500

    Beta

--- api/v1/quotes.js
+++ api/v1/quotes.js
@@ -4,21 +4,17 @@ const axios = require("axios");
 
 module.exports = async (req, res) => {
 
-
   const url = `https://api.coinranking.com/v1/public/coin/1/history/30d`
-    
-  const query_quotes = await axios.get( url )
-  .catch( error => res.status(500).json( {status:"error", error} ) );
 
-  const quotes = query_quotes.data.data.history.filter( x => (new Date(x.timestamp)).getHours() == 0 )    
+  const query_quotes = await axios.get(url)
+    .catch(error => res.status(500).json({ status: "error", error }));
+
+  const daily_quotes = query_quotes.data.data.history.filter(x => (new Date(x.timestamp)).getHours() == 0)
 
   res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
 
-  res.status(200).json( { quotes } );
+  res.status(200).json({ quotes: daily_quotes });
 };
-
-
-
--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,16 @@
 <script>
   import axios from "axios";
 
-  const day_names = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  
+  const day_names = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
   let previous_price_click = 0;
 
   let quote_clicked;
@@ -12,69 +20,71 @@
   axios
     // .get("http://localhost:3000/api/v1/quotes")
     .get("https://btcprices.vercel.app/api/v1/quotes")
-    .then( payload => {
+    .then((payload) => {
       quotes = payload.data.quotes;
 
-      quotes.sort( (a,b) => a.timestamp - b.timestamp )
-
+      quotes.sort((a, b) => a.timestamp - b.timestamp);
     });
 
+  function handleRowClick(i) {
+ 
+    const url = `http://www.example.com/save_hits?priceDate=${quotes[i].timestamp}&priceClick=${quotes[i].price}&previousPriceClick=${previous_price_click || "na"}`;
 
-function handleRowClick(i) {
+    quote_clicked = i;
 
-  console.log(i)
+    axios
+      .get(url)
+      .then((x) => console.log(x))
+      .catch((e) => {
+        console.log("Error:", e);
+      });
 
-  const url = `http://www.example.com/save_hits?priceDate=${quotes[i].timestamp}&priceClick=${quotes[i].price}&previousPriceClick=${ previous_price_click || 'na' }`
-
-  quote_clicked = i
-
-  axios.get( url  )
-  .then(  x => console.log(x) )
-  .catch( e => { 
-    console.log( 'Error:',e)
-  } 
-    
-  )
-
-  previous_price_click = quotes[i].price
-}
+    previous_price_click = quotes[i].price;
+  }
 </script>
 
 <main>
   <div class="uk-container">
-    <h1 class="uk-heading-small">Bitcoin Price History</h1>
+    <h1 class="uk-heading-small uk-heading-divider">Bitcoin Price History</h1>
 
-    <table class="uk-table uk-table-small uk-table-divider uk-table-responsive uk-table-middle">
+    <table
+      class="uk-table uk-table-small uk-table-divider uk-table-middle"
+    >
       <thead>
         <tr>
           <th class="uk-width-1-6">Date</th>
           <th class="uk-width-1-6">Day of Week</th>
-          <th class="uk-width-1-6">Price</th>
-          <th class="uk-width-1-6">Change</th>
+          <th class="uk-width-1-6">Price (US$)</th>
+          <th class="uk-width-1-6">Change (US$)</th>
           <th class="uk-width-1-6">Direction</th>
         </tr>
       </thead>
       <tbody>
         {#each quotes as quote, i}
-          <tr class="uk-text-left"  class:uk-active="{quote_clicked === i}"  on:click={ () =>  { handleRowClick(i) } }  >
-            <td> { new Date(quote.timestamp).toISOString().split('T')[0] + "T00:00:00" }   </td>
-            <td> { day_names[ new Date(quote.timestamp).getDay()  ]   } </td>
-            <td> {quote.price}   </td>
-            <td> { ( i === 0 ) ?  'N/A' : (quotes[i].price - quotes[i-1].price)   }     </td>
-            <td class="uk-align-center"> 
-              {#if i === 0} 
-                N/A
-              {:else if ( (quotes[i].price - quotes[i-1].price) > 0)   }
-                <span style="color:green" uk-icon="arrow-up"></span>
-              {:else if ( (quotes[i].price - quotes[i-1].price) < 0)   }
-                <span style="color:red"  uk-icon="arrow-down"></span>
-              {:else}
-                <span uk-icon="minus"></span>
-
-              {/if}
-            
+          <tr
+            class="uk-text-left"
+            class:uk-active={quote_clicked === i}
+            on:click={() => {
+              handleRowClick(i);
+            }}
+          >
+            <td>
+              {new Date(quote.timestamp).toISOString().split("T")[0] + "T00:00:00"}
             </td>
-          
+            <td> {day_names[new Date(quote.timestamp).getDay()]} </td>
+            <td> {quote.price} </td>
+            <td> {i === 0 ? "N/A" : quotes[i].price - quotes[i - 1].price} </td>
+            <td class="uk-align-center">
+              {#if i === 0}
+                N/A
+              {:else if quotes[i].price - quotes[i - 1].price > 0}
+                <span style="color:green" uk-icon="arrow-up" />
+              {:else if quotes[i].price - quotes[i - 1].price < 0}
+                <span style="color:red" uk-icon="arrow-down" />
+              {:else}
+                <span uk-icon="minus" />
+              {/if}
+            </td>
           </tr>
         {/each}
       </tbody>
